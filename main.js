d15390f35d0048366878064204177150287d9fc0
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-03-06 09:38:30 -0500

    evance 1

---
+++ main.js
@@ -0,0 +1,19 @@
+/* function cargar(){
+    alert("hola")
+} */
+/* 
+addEventListener("DONContendloade", function(event){
+    console.log("holA")
+})
+
+
+let Nombre="jhon";
+let edad=23;
+
+
+console.log(Nombre);
+console.table([{nombre: nombre}])
+console.table({edad: ${edad}})
+
+document.querySelector("body").insertAdjacentHTML("beforeend",edad);
+ */
---
+++ taller_1.js
@@ -0,0 +1,31 @@
+const standar = {
+    traditional:["90-100", "80-89","70-79","60-69","<+60","0"],
+    Point :["12-14","9-11","6-8","3-5","1-2","0"],
+    Letter:["A","B","C","D","E","F"],
+    SBGRating:[4,3,2,1,0,0],
+    Proficiency_level_with_Standard:["resive un viaje a dubay","Resive una Toyota","resive una moto","resive un cauderno","debe pagar un vargo","paga toda la carrera"],
+};
+
+var tipo;
+
+tipo = prompt("Introduce la forma de calificacion (traditional = 1 , Point = 2 , Letter = 3): ");
+
+
+console.table(standar);
+let den=0;
+
+
+
+
+switch(den){
+    case 0:
+    if(tipo == 1 ){
+        console.log("hola");
+    };
+    break;
+    case 1:
+
+}
+
+    
+
